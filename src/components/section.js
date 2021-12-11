621e50f1cd39a78ad73386f5990bb70247628af8
Chris Austin <ca.austin+github@gmail.com>
2021-12-11 19:15:00 +0000

    new pics and styling

--- src/components/section.js
+++ src/components/section.js
@@ -2,50 +2,76 @@ import * as React from "react";
 import styled from "styled-components";
 
 let Container = styled.div`
-  background: #0B1D26;
+  background: #0b1d26;
   position: relative;
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  height: 100%;
+  margin: auto;
 `;
+
 let Number = styled.div`
   font-size: 15em;
   font-style: italic;
   font-weight: 700;
-  opacity: .1;
+  opacity: 0.1;
   font-family: Queens;
   position: absolute;
   top: 0;
   left: 0;
   transform: translateX(-50%) translateY(-50%);
 `;
-let Image = styled.div``;
+
+let Image = styled.img`
+  max-width: 565px;
+  height: 720px;
+  position: relative;
+`;
+
 let Subtitle = styled.div`
   color: #549353;
   font-size: 1.125em;
   text-transform: uppercase;
-  letter-spacing: .5em;
+  letter-spacing: 0.5em;
 `;
 
 let Title = styled.div`
   font-family: Queens;
   font-size: 4em;
 `;
+
 let Description = styled.div`
-  font-size: 1.125em;
+  font-size: 1em;
+  flex-shrink: 3;
+  font-weight: 300;
+  letter-spacing: 0.03em;
 `;
+
 let Action = styled.div`
   color: #549353;
+`;
+
+let TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 150px;
+  max-width: 650px;
 `;
 
 const Section = ({ number, image, subtitle, title, description, action }) => {
   return (
     <Container>
-      <Number>{number}</Number>
-      <Image>{image}</Image>
-      <Subtitle>{subtitle}</Subtitle>
-      <Title>{title}</Title>
-      <Description>{description}</Description>
-      <Action>{action}</Action>
+      <TextBox>
+        <Number>{number}</Number>
+        <Subtitle>{subtitle}</Subtitle>
+        <Title>{title}</Title>
+        <Description>{description}</Description>
+        <Action>{action}</Action>
+      </TextBox>
+      <Image src={image}></Image>
     </Container>
-  )
-}
+  );
+};
 
-export default Section
+export default Section;
